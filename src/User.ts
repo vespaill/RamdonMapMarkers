de84218c2d9c628d53f5bf3b8e2199a97fc90ae4
Victor Espaillat <vicander_99@hotmail.com>
2020-05-16 07:35:07 -0400

    Google Maps Integration

---
+++ src/User.ts
@@ -0,0 +1,19 @@
+import faker from "faker";
+
+export class User {
+    name: string;
+    location: {
+        lat: number;
+        lng: number;
+    };
+
+    constructor() {
+        this.name = faker.name.firstName();
+
+        /* Location was not yet defined, so lat and lng didn't exist yet. */
+        this.location = {
+            lat: parseFloat(faker.address.latitude()),
+            lng: parseFloat(faker.address.longitude())
+        };
+    }
+}
